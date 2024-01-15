3ce7bfc47795c02893d45b6af2231a55c1d7234e
t_jahoda <t_jahoda@utb.cz>
2024-01-15 22:17:22 +0100

    Initial Commit

---
+++ FinancialHelper/FinancialHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialHelperApp.swift
+//  FinancialHelper
+//
+//  Created by Tomáš Jahoda on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinancialHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
